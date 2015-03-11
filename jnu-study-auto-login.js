9243d86cf8664db943e6b0eff881bac92c5b48ad
DengZuoheng <dengzuoheng@gmail.com>
2015-03-12 00:19:40 +0800

    study-auto-login

---
+++ jnu-study-auto-login.js
@@ -0,0 +1,13 @@
+﻿// ==UserScript==
+// @name         jnu study auto login
+// @namespace    http://dengzuoheng.github.io
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://study.jnu.edu.cn/
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+$("#user_id").val("2012052207");
+$("#password").val("2012052207");
+$("input[src='/webapps/bb-silkIII-bb_bb60/homepage/images/login_btn.jpg']").click();
