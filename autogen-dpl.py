5f2cf95c04c1471541f87232b33205beb69e988c
DengZuoheng <dengzuoheng@gmail.com>
2015-04-24 21:41:49 +0800

    add autogen-dpl

---
+++ autogen-dpl.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os,sys
+import re
+import random
+filename_list = os.listdir(os.getcwd()) 
+music = []
+for item in filename_list:
+    path = os.path.join(os.getcwd(),item)
+    if os.path.isdir(path):
+        continue
+    r = re.findall(r'\.(dpl|txt|py)$',item)
+    if(len(r)>0):
+        continue
+    music.append(item)
+
+playlist_file = open('0-playlist.dpl','w')
+playlist_file.write('DAUMPLAYLIST\n')
+begin = random.randint(0,len(music))
+playlist_file.write('playname=%s\n'%os.path.join(os.getcwd(),music[begin]))
+playlist_file.write('%d\n'%begin)
+playlist_file.write('topindex=0\n')
+i = 0
+for item in music:
+    playlist_file.write('%d*file*%s\n'%(i,item))
+    playlist_file.write('%d*played*0\n'%i)
+    i = i+1
+    
+
+
