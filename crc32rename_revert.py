6457a934ab143ef2d315a988aef0296cd51b8a39
DengZuoheng <dengzuoheng@gmail.com>
2015-07-07 15:05:34 +0800

    rename_revert改用with语句

--- crc32rename_revert.py
+++ crc32rename_revert.py
@@ -5,10 +5,10 @@ import os,sys
 import re
 import json
 
-f = open('log','r')
-file_name_log = json.load(f)
-for item in file_name_log:
-    for key in item:
-        old_name = key
-        new_name = item[key]
-        os.rename(new_name,old_name)
+with open('log','r') as log_file:
+    file_name_log = json.load(log_file)
+    for item in file_name_log:
+        for key in item:
+            old_name = key
+            new_name = item[key]
+            os.rename(new_name,old_name)
